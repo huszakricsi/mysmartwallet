e83fafe566ad809e2972bc5e51e2d6011e1dc0c9
Huszák Richárd János <huszakricsi@gmail.com>
2018-11-27 15:44:33 +0100

    Language settings stored locally.

--- src/app/components/toolbar/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/state/auth.service';
+import { LocalStorageService } from 'angular-web-storage';
 
 @Component({
   selector: 'app-toolbar',
@@ -13,7 +14,7 @@ export class ToolbarComponent implements OnInit {
 
   location:string = 'home';
 
-  constructor(private router:Router, private authService:AuthService) { }
+  constructor(private router:Router, private authService:AuthService, public local: LocalStorageService) { }
 
   ngOnInit() {
   }
@@ -25,6 +26,7 @@ export class ToolbarComponent implements OnInit {
   }
   public setLanguage(language:string)
   {
+    this.local.set('language',language);
     this.drawer.toggle();
   }
   public logOut(){
